76697ddba78ceacedd93275b6e93f104e24c5c7b
Jose Garcia <joseorozco9jdm@gmail.com>
2021-03-07 20:03:36 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio_Dos_Repaso/DATOS_VEHICULO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_Dos_Repaso
+{
+    class DATOS_VEHICULO
+    {
+        string placa;
+        string marca;
+        string modelo;
+        string color;
+        double precio_kilometro;
+
+    }
+}
